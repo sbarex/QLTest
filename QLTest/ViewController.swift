4595e0ad7238548e464f4ae6f619efb1a0424073
sbarex <sbarex@gmail.com>
2020-12-10 20:34:20 +0100

    Initial Commit

---
+++ QLTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QLTest
+//
+//  Created by Simone Baldissini on 10/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
